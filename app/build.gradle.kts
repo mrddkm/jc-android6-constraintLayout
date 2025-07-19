969a779597d58d389388af8523a0a376fa492a9c
Didik Muttaqien <mrddkm@gmail.com>
2025-07-19 09:02:28 +0700

    update custom name apk released

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,29 @@
+import java.io.FileInputStream
+import java.time.Clock
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Properties
+import java.util.TimeZone
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
+
+val buildProperties = Properties()
+val buildPropertiesFile = rootProject.file("build.properties")
+if (buildPropertiesFile.exists()) {
+    buildProperties.load(FileInputStream(buildPropertiesFile))
+}
+val appVersionCode = buildProperties.getProperty("versionCode", "1").toInt()
+val appVersionName: String? = buildProperties.getProperty("versionName", "1.0.0")
+val appProductName: String? = buildProperties.getProperty("productName", "arkhe")
+val now: Instant? = Clock.systemUTC().instant()
+val localDateTime: LocalDateTime? = now?.atZone(TimeZone.getDefault().toZoneId())?.toLocalDateTime()
+val timestampFormatter: DateTimeFormatter? = DateTimeFormatter.ofPattern("yyMMddHHmm")
+val timestampString = localDateTime?.format(timestampFormatter)
+val buildTimestamp = timestampString
 
 android {
     namespace = "com.jc.constraintlayout"
@@ -11,8 +33,8 @@ android {
         applicationId = "com.jc.constraintlayout"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = appVersionCode
+        versionName = "$appVersionName.build$buildTimestamp"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -50,6 +72,10 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+base {
+    archivesName.set("$appProductName-$appVersionName.build$buildTimestamp")
 }
 
 dependencies {
