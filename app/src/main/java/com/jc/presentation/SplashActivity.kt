56137b545faa727e4a3390c17d6e633fa798224e
Didik Muttaqien <mrddkm@gmail.com>
2025-07-21 04:24:28 +0700

    add Custom Splash Screen

---
+++ app/src/main/java/com/jc/presentation/SplashActivity.kt
@@ -0,0 +1,92 @@
+package com.jc.presentation
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.jc.constraintlayout.R
+import com.jc.presentation.ui.theme.ConstraintLayoutTheme
+import kotlinx.coroutines.delay
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ConstraintLayoutTheme {
+                CustomSplashScreen()
+            }
+        }
+    }
+
+    @Composable
+    private fun CustomSplashScreen() {
+        val alpha = remember {
+            Animatable(0f)
+        }
+        LaunchedEffect(key1 = true) {
+            alpha.animateTo(
+                targetValue = 1f,
+                animationSpec = tween(1500)
+            )
+            delay(timeMillis = 2000)
+            startActivity(
+                Intent(
+                    this@SplashActivity,
+                    MainActivity::class.java
+                )
+            )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    modifier = Modifier
+                        .alpha(alpha.value),
+                    painter = painterResource(id = R.drawable.splash_ic),
+                    contentDescription = null
+                )
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun SplashPreview() {
+        ConstraintLayoutTheme {
+            CustomSplashScreen()
+        }
+    }
+}
+
