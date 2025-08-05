0825e6bf5332300078b9c1a58559fccab6689266
mrddkm <mrddkm@gmail.com>
2025-08-05 15:36:17 +0700

    refactor project-name

--- app/src/main/java/com/arkhe/Base.kt
+++ app/src/main/java/com/arkhe/Base.kt
@@ -7,13 +7,13 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
-class Arkhe : Application() {
+class Base : Application() {
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
             androidLogger(Level.ERROR)
-            androidContext(this@Arkhe)
+            androidContext(this@Base)
             modules(appModule)
         }
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,5 +21,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ConstraintLayout"
+rootProject.name = "arkhe-base"
 include(":app")
